4f4744776f2af00fae8078356e45dfcca4f828e8
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-04 15:15:51 +0100

    add load log, add sources

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,36 @@ function HomePage({ allFeeds }) {
 
 export default HomePage;
 
+export const getStaticProps = async () => {
+
+  const sources = {
+    "知乎每日精选" : "https://www.zhihu.com/rss",
+    "知乎日报" : "https://rsshub.app/zhihu/daily",
+    "新浪创事记" : "https://rsshub.app/sina/csj",
+    "央视新闻" : "https://rsshub.app/cctv/xwlb",
+    "外交动态" : "https://rsshub.app/gov/mfa/wjdt/fyrbt",
+    "GitHub" : "https://github.com/sugarmanzhu.atom",
+    "The Verge" : "https://www.theverge.com/rss/frontpage",
+    // "微软亚洲研究院" : "https://msra.cn/feed",
+    // "MIT Technology Review" : "https://www.technologyreview.com/feed/",
+    // "TechCrunch" : "https://techcrunch.com/feed",
+    // "Mashable" : "https://mashable.com/feeds/rss/all",
+    // "Wired" : "https://www.wired.com/feed/rss",
+    // "The New York Times" : "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
+    // "BBC News" : "http://feeds.bbci.co.uk/news/technology/rss.xml",
+  }
+
+  const fetch_time = new Date().getTime();
+  const allFeeds = await getFeeds(sources, 50, fetch_time);
+  
+  return {
+    props: {
+      allFeeds,
+    },
+    revalidate: 10,
+  };
+}
+
 function timePassed(rss_time, fetch_time) {
 
   const diff = fetch_time - rss_time;
@@ -44,48 +74,28 @@ async function getFeeds(rss_sources, limit, fetch_time) {
   const parser = new Parser();
   let feeds = [];
   for (const [source, link] of Object.entries(rss_sources)) {
-    const rss_feeds = await parser.parseURL(link);
-    for (const item of rss_feeds.items) {
-      const time_string = new Date(Date.parse(item.isoDate)).toUTCString();
-      const rss_time = Date.parse(item.isoDate);
-      const time_passed = timePassed(rss_time, fetch_time);
-      feeds.push({
-        "title" : item.title,
-        "link" : item.link,
-        "isoDate" : item.isoDate,
-        "time_string" : time_string,
-        "time_passed" : time_passed,
-        "content" : item.contentSnippet,
-        "provider" : source,
-      });
+    try {
+      const rss_feeds = await parser.parseURL(link);
+      for (const item of rss_feeds.items) {
+        const time_string = new Date(Date.parse(item.isoDate)).toUTCString();
+        const rss_time = Date.parse(item.isoDate);
+        const time_passed = timePassed(rss_time, fetch_time);
+        feeds.push({
+          "title" : item.title,
+          "link" : item.link,
+          "isoDate" : item.isoDate,
+          "time_string" : time_string,
+          "time_passed" : time_passed,
+          "content" : item.contentSnippet,
+          "provider" : source,
+        });
+      }
+      console.log(`Loaded ${source}`);
+    } catch (error) {
+      console.log(`Error while loading ${source}: ${error}`);
     }
   }
   feeds.sort((a, b) => b.isoDate.localeCompare(a.isoDate))
 
   return feeds.slice(0, limit);
-}
-
-export const getStaticProps = async () => {
-
-  const sources = {
-    "知乎每日精选" : "https://www.zhihu.com/rss",
-    "微软亚洲研究院" : "https://msra.cn/feed",
-    "MIT Technology Review" : "https://www.technologyreview.com/feed/",
-    "TechCrunch" : "https://techcrunch.com/feed",
-    "The Verge" : "https://www.theverge.com/rss/frontpage",
-    "Mashable" : "https://mashable.com/feeds/rss/all",
-    "Wired" : "https://www.wired.com/feed/rss",
-    "The New York Times" : "https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml",
-    "BBC News" : "http://feeds.bbci.co.uk/news/technology/rss.xml",
-  }
-
-  const fetch_time = new Date().getTime();
-  const allFeeds = await getFeeds(sources, 50, fetch_time);
-  
-  return {
-    props: {
-      allFeeds,
-    },
-    revalidate: 10,
-  };
 }
