974b736d6d57e83a06b765fb5c16e97c8e8b8a6f
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-04 04:20:27 +0100

    Sync time between client and server

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,59 @@
 import Parser from "rss-parser";
 import RSSFeeds from "../components/RSSFeeds";
 
-function HomePage({ allFeeds, time }) {
-  return <><h1>ISR - {time}</h1>
-  <RSSFeeds feeds={allFeeds} /></>
+function HomePage({ allFeeds, fetch_time }) {
+  return <><h1>ISR - {fetch_time}</h1>
+  <RSSFeeds feeds={allFeeds}/></>
 }
 
 export default HomePage;
 
-async function getFeeds(rss_sources, limit) {
+function timePassed(time, fetch_time) {
+  const date = new Date(Date.parse(time));
+  const diff = fetch_time - date;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  if (days > 1) {
+      return days + " days ago";
+  } else if (days === 1) {
+      return "1 day ago";
+  } 
+
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  if (hours > 1) {
+      return hours + " hours ago";
+  } else if (hours === 1) {
+      return "1 hour ago";
+  } 
+
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  if (minutes > 1) {
+      return minutes + " minutes ago";
+  } else if (minutes === 1) {
+      return "1 minute ago";
+  } 
+  
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  if (seconds > 1) {
+      return seconds + " seconds ago";
+  } else if (seconds === 1) {
+      return "1 second ago";
+  }
+}
+
+async function getFeeds(rss_sources, limit, fetch_time) {
   const parser = new Parser();
   let feeds = [];
   for (const [source, link] of Object.entries(rss_sources)) {
     const rss_feeds = await parser.parseURL(link);
     for (const item of rss_feeds.items) {
       const date = item.isoDate.substring(0, 10) + " " + item.isoDate.substring(11, 19);
+      const time_passed = timePassed(date, fetch_time);
       feeds.push({
         "title" : item.title,
         "link" : item.link,
         "date" : date,
+        "time_passed" : time_passed,
         "content" : item.contentSnippet,
         "provider" : source,
       });
@@ -30,7 +65,7 @@ async function getFeeds(rss_sources, limit) {
 }
 
 export const getStaticProps = async () => {
-  console.log("Hi");
+  
   const sources = {
     "知乎每日精选" : "https://www.zhihu.com/rss",
     "微软亚洲研究院" : "https://msra.cn/feed",
@@ -43,12 +78,13 @@ export const getStaticProps = async () => {
     "BBC News" : "http://feeds.bbci.co.uk/news/technology/rss.xml",
   }
 
-  const allFeeds = await getFeeds(sources, 50);
-  const time = Date.now();
+  const fetch_time = Date.now();
+  const allFeeds = await getFeeds(sources, 50, fetch_time);
+  
   return {
     props: {
       allFeeds,
-      time,
+      fetch_time,
     },
     revalidate: 10,
   };
