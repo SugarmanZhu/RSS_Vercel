72f4d076709ae0c99d8c7a51d9680db3ac4dfe25
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-06 20:45:15 +0100

    Fix wrong bottom fade transition duration

--- components/RSSList.tsx
+++ components/RSSList.tsx
@@ -17,13 +17,11 @@ function RSSList({ feeds }) {
             {/* bottom fades */}
             <div 
                 className="fixed bottom-0 left-0 w-full h-28 transition-all 
-                duration-200 bg-gradient-to-t from-black opacity-0 
-                dark:opacity-100" 
+                bg-gradient-to-t from-black opacity-0 dark:opacity-100" 
             />
             <div 
                 className="fixed bottom-0 left-0 w-full h-28 transition-all 
-                duration-200 bg-gradient-to-t from-white opacity-100 
-                dark:opacity-0" 
+                bg-gradient-to-t from-white opacity-100 dark:opacity-0" 
             />
         </Fragment>
     )
