d4ba6b3ccc4aa46660a2b33268777f16cfa767ac
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-06 21:32:07 +0100

    Adjust colors to be consistent, add configs

--- components/RSSList.tsx
+++ components/RSSList.tsx
@@ -17,11 +17,11 @@ function RSSList({ feeds }) {
             {/* bottom fades */}
             <div 
                 className="fixed bottom-0 left-0 w-full h-28 transition-all 
-                bg-gradient-to-t from-black opacity-0 dark:opacity-100" 
+                bg-gradient-to-t from-neutral-800 opacity-0 dark:opacity-100" 
             />
             <div 
                 className="fixed bottom-0 left-0 w-full h-28 transition-all 
-                bg-gradient-to-t from-white opacity-100 dark:opacity-0" 
+                bg-gradient-to-t from-neutral-50 opacity-100 dark:opacity-0" 
             />
         </Fragment>
     )
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     // default dark scheme
     <Html className='dark p-0 m-0'>
       <Head />
-      <body className='m-0 bg-gray-50 dark:bg-neutral-800 text-neutral-900 
-        dark:text-neutral-50 transition-colors'>
+      <body className='m-0 bg-neutral-50 dark:bg-neutral-800 
+        text-neutral-900 dark:text-neutral-50 transition-colors'>
         <Main />
         <NextScript />
       </body>
