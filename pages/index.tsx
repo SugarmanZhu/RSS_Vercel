7e1640d81d43310e067cac7310967483c04a8063
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-07 02:44:11 +0100

    start fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,8 +35,7 @@ export default HomePage;
 
 export const getStaticProps = async () => {
   // get 50 lastest RSS feeds from API (limited to 50 for performance)
-  // const feeds = await (await fetch(`${server}/api/50`)).json();
-  const feeds = [];
+  const feeds = await (await fetch(`${server}/api/50`)).json();
   return {
     props: {
       feeds,
