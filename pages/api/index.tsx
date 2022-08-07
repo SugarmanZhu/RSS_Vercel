a33a2e0fd604d0ffa1a353d61f8f1a12679747b6
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-07 07:28:46 +0100

    Update api no param message

--- pages/api/index.tsx
+++ pages/api/index.tsx
@@ -5,5 +5,7 @@ export default async function handler(
   req : Request, res : Response
 ) : Promise<void> {
   res.status(200).json({message: `Hello from ${server}/api. 
-  Please use ${server}/api/[limit] to fetch RSS feeds.`});
+  Please use ${server}/api/[number of feeds] to fetch RSS feeds.
+  For more information visit 
+  https://github.com/SugarmanZhu/RSS_Vercel/blob/main/README.md#api-usage`});
 }
