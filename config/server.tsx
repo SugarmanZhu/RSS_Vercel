1bd7585c7e06ab09cc4e0f6122c4e7004e8f6680
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-20 12:29:48 +0100

    update server address zhuxiaotan.com

--- config/server.tsx
+++ config/server.tsx
@@ -1,4 +1,4 @@
 const dev : boolean = process.env.NODE_ENV !== 'production';
 
 export const server : string = 
-    dev ? 'http://localhost:3000' : 'https://rss.zhuxiaotan.xyz';
+    dev ? 'http://localhost:3000' : 'https://zhuxiaotan.com';
