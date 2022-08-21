dc6ccaeb3519664aca95708235341acf447658ac
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-21 07:27:43 +0100

    Add smooth scroll (nice ctrl-f animation)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     // default dark scheme
-    <Html className='dark p-0 m-0'>
+    <Html className='dark p-0 m-0 scroll-smooth'>
       <Head />
       <body className='m-0 bg-neutral-50 dark:bg-neutral-800 
         text-neutral-900 dark:text-neutral-50 transition-colors'>
