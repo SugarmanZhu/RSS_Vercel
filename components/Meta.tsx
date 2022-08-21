52543176c310f9224c0f6260d5e7eebdef205ec5
Xiaotan Zhu <zxt@zhuxiaotan.xyz>
2022-08-21 22:22:40 +0100

    Add apple touch icon

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -4,12 +4,14 @@ interface MetaProps {
   title: string;
   description: string;
   favicon?: string;
+  appleicon?: string;
 }
 
 export default function Meta({ 
   title = "Xiaotan", 
   description = "Xiaotan's Page", 
-  favicon = "/favicon.ico" }: MetaProps
+  favicon = "/favicon.ico",
+  appleicon = "/apple-touch-icon.png" }: MetaProps
 ) {
   return (
     <Head>
@@ -17,6 +19,7 @@ export default function Meta({
       <meta charSet="utf-8" />
       <meta name="description" content={description} />
       <link rel="icon" href={favicon} />
+      <link rel="apple-touch-icon" href={appleicon} />
       <title>{title}</title>
     </Head>
   );
